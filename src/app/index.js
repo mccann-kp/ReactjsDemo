7af00438ac01e99deeb48df8cfb33aef45ddec74
Kieran McCann <kp.mccann@outlook.com>
2016-11-01 10:54:19 -0500

    final update before router

--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,8 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      homeLink: "Home"
+      homeLink: "Home",
+      homeMounted: true
     }
   }
 
@@ -23,21 +24,39 @@ class App extends React.Component {
     })
   }
 
+  onChangeHomeMounted(){
+    this.setState({
+      homeMounted: !this.state.homeMounted
+    })
+  }
   render() {
+    let homeCmp = ""
+    if (this.state.homeMounted){
+      homeCmp = (
+        <Home
+          name={"Max"}
+          initialAge={27}
+          greet={this.onGreet}
+          changeLink={this.onChangeLinkName.bind(this)}
+          initialLinkName={this.state.homeLink}
+        />
+      )
+    }
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
             <Header homeLink={this.state.homeLink}/>
           </div>
+        </div>
+        <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home
-              name={"Max"}
-              initialAge={27}
-              greet={this.onGreet}
-              changeLink={this.onChangeLinkName.bind(this)}
-              initialLinkName={this.state.homeLink}
-              />
+            {homeCmp}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>
           </div>
         </div>
       </div>
